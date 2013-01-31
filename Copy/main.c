f9f737e066c1499db3166c2339fc4dea4c578a28
Gary Johnson <logic@Logics-MacBook-Pro.local>
2013-01-30 23:31:10 -0500

    Initial Commit

---
+++ Copy/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Copy
+//
+//  Created by Gary Johnson on 1/30/13.
+//  Copyright (c) 2013 this.that. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
